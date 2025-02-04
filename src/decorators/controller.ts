ab559e3033b290ba6ba12bac8eba9139a46b1a2b
Kylian-Mallet <kylian-mallet@hotmail.fr>
2025-02-04 16:30:00 +0100

    ✨ feat(decorators): Add a new `@Controller()` decorator to prefix methods

---
+++ src/decorators/controller.ts
@@ -0,0 +1,33 @@
+import { REFLECT_ROUTES_KEY } from '../constants.js'
+
+/**
+ * Decorator for applying middleware to a route in AdonisJS v6
+ * @param pattern The route pattern
+ * @param prefix Optional prefix for the controller
+ * @returns A decorator function
+ * @example
+ * // In an AdonisJS v6 controller:
+ * @Controller('/users')
+ * export default class UserController {
+ *  @Get('/list')
+ *  async index({ response }: HttpContext) {
+ *    // Handle GET request for /users/list
+ *  }
+ * }
+ */
+export const Controller = (pattern: string, prefix?: string) => {
+  return (target: any) => {
+    console.log('Registering controller =>', pattern, prefix)
+    const routes = Reflect.getMetadata(REFLECT_ROUTES_KEY, target) || {}
+
+    for (const key in routes) {
+      routes[key] = {
+        ...routes[key],
+        pattern: `${pattern}${routes[key].pattern}`,
+        name: routes[key].name && prefix ? `${prefix}.${routes[key].name}` : routes[key].name,
+      }
+    }
+
+    Reflect.defineMetadata(REFLECT_ROUTES_KEY, routes, target)
+  }
+}
